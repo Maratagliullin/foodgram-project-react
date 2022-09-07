74e69569863644eec437e859dfd93ff717a4bae7
Marat Agliullin <marat.agl@gmail.com>
2022-09-07 21:33:39 +0300

    change management commands

--- backend/foodgram/run_management_commands.sh
+++ backend/foodgram/run_management_commands.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python manage.py rename_app food foods
 python manage.py makemigrations foods users
 python manage.py migrate 
 python manage.py collectstatic --no-input
@@ -7,5 +6,4 @@ python manage.py createsuperuser \
         --noinput \
         --username $DJANGO_SUPERUSER_USER \
         --email $DJANGO_SUPERUSER_EMAIL
-# exec gunicorn foodgram.wsgi:application --bind 0:8000
-exec python3 manage.py runserver 0:8000
+exec gunicorn foodgram.wsgi:application --bind 0:8000
