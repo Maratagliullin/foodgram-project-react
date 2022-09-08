d8ddfc59b52fe43909e9e4137b0bb9e56dd1c11a
Marat Agliullin <marat.agl@gmail.com>
2022-09-08 16:48:28 +0300

    fix url fovorites path

--- backend/foodgram/foods/urls.py
+++ backend/foodgram/foods/urls.py
@@ -10,7 +10,7 @@ app_name = 'foods'
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('recipes/favorites', FavoriteViewSet, basename='favorites')
+router.register('favorites', FavoriteViewSet, basename='favorites')
 router.register('tags', TagViewSet, basename='tags')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 
